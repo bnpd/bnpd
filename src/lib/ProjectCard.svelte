a9baa2a367c31c2c2b4cbd14db3b858236cf604a
bnpd <benjamin.paddags@gmail.com>
2025-06-25 23:28:51 +0200

    svelte working with ProjectCard component

---
+++ src/lib/ProjectCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let link: string = ""; // main project link
+  export let repo: string = ""; // repo link
+  export let demoUrl: string = ""; // iframe demo url
+  export let demoVideo: string = ""; // video src
+
+  let isDemoOn = false;
+</script>
+
+<div class="card rounded padding">
+  <h2 class="text-center">{title}</h2>
+  <p>{description}</p>
+  <slot name="extra" />
+  {#if demoUrl && isDemoOn}
+    <iframe class="rounded" src={demoUrl} title="Demo"></iframe>
+  {/if}
+  {#if demoVideo && !isDemoOn}
+    <video class="rounded" controls autoplay muted>
+      <source src={demoVideo} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  {/if}
+  <div class="text-center space-evenly">
+    {#if demoUrl && demoVideo}
+      {#if isDemoOn}
+        <button class="blue" on:click={() => (isDemoOn = false)}>Show Video</button>
+      {:else}
+        <button class="blue" on:click={() => (isDemoOn = true)}>Try it live</button>
+      {/if}
+    {/if}
+    {#if link}
+      <a target="_blank" href={link} class="blue button no-underline">Try it</a>
+    {/if}
+    {#if repo}
+      <a target="_blank" href={repo} class="blue button no-underline">Explore Code</a>
+    {/if}
+  </div>
+  <div class="margin-top">
+      <slot name="footer" />
+  </div>
+</div>
